c92c458e2beb8aa20abfd7add9f7d1318856c692
imdeepss <imdeepsv@gmail.com>
2026-01-12 12:09:17 +0530

    fix:                     Leadership driving progress

--- pages/about.php
+++ pages/about.php
@@ -478,25 +478,25 @@ $lubricant_features = [
 
         <div class="swiper who-we-are-swiper !pt-8 md:block block" id="about-who-we-are-swiper">
             <div class="swiper-wrapper md:grid md:grid-cols-5">
-                            <?php foreach ($lubricant_features as $feature): ?>
+                <?php foreach ($lubricant_features as $feature): ?>
                     <div
                         class="swiper-slide group relative bg-y100 md:h-[204px] !h-[204px] p-3 transition-all duration-500 ease-[cubic-bezier(0.4,0,0.2,1)] hover:-translate-y-4 !mt-4 cursor-pointer">
 
                         <h6
                             class="text-[#3B3B3B] font-base font-normal text-[20px] leading-[140%] md:text-[18px] md:leading-[140%] mb-2 relative z-10">
-                                        <?php echo $feature['title']; ?>
+                            <?php echo $feature['title']; ?>
                         </h6>
 
                         <p
                             class="text-[#666666] font-base font-normal text-[14px] md:text-[12px] leading-[150%] md:leading-[150%] tracking-[0.015em] relative z-10">
-                                        <?php echo $feature['desc']; ?>
+                            <?php echo $feature['desc']; ?>
                         </p>
 
                         <div
                             class="absolute bottom-0 left-0 h-[3px] w-full bg-[#1A3B1B] origin-left scale-x-0 transition-transform duration-500 ease-out group-hover:scale-x-100 z-20">
                         </div>
                     </div>
-                            <?php endforeach; ?>
+                <?php endforeach; ?>
             </div>
 
             <div class="flex md:hidden items-center gap-4 relative z-10 justify-end mt-6">
@@ -612,7 +612,8 @@ $lubricant_features = [
                 <p class="text-[#757575] font-base font-normal text-[16px] leading-[150%] tracking-[0.015em]">
                     Our commitment remains unchanged: to keep industries moving, machines performing and our customers
                     ahead
-                    of what’s next as their performance multiplier
+                    of what’s next as their <span class="text-[#F4C300] font-bold capitalize">performance
+                        multiplier</span>
                 </p>
                 <div class="flex items-start gap-1 flex-col">
                     <p
