5dd1a7ecd90401a5f4382c922d8ae636f193fe5e
imdeepss <deepakk@geeconglobal.com>
2026-01-17 05:29:14 +0000

    replace config setting

--- includes/config.php
+++ includes/config.php
@@ -3,31 +3,32 @@
 
 // Site Configuration
 define('SITE_NAME', 'MOSIL Lubricants');
-define('SITE_URL', 'http://localhost/mosil-new');
+define('SITE_URL', 'https://prpartner.in');
 define('BASE_URL', SITE_URL . '/');
+
 
 // Database Configuration (Placeholder)
 //localhost db config
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'u698941191_mosil');
+//define('DB_HOST', 'localhost');
+//define('DB_USER', 'root');
+//define('DB_PASS', '');
+//define('DB_NAME', 'u698941191_mosil');
 
 
 // live db config
-// define('DB_HOST', '89.117.157.162');
-// define('DB_USER', 'u284616336_mosil_new');
-// define('DB_PASS', 'Ruchi@2026');
-// define('DB_NAME', 'u284616336_mosil_new');
+ define('DB_HOST', 'localhost');
+ define('DB_USER', 'u284616336_mosil_new');
+ define('DB_PASS', 'Ruchi@2026');
+ define('DB_NAME', 'u284616336_mosil_new');
 
 // Error Reporting (Turn off for production)
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
 // Session Configuration
 define('SESSION_TIMEOUT', 1800); // 30 minutes
 
 // Database Connection
 require_once 'db.php';
 ?>
+
