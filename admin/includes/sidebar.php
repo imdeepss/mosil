31fa473b2cab943beaef7480d4295eb8d46af152
imdeepss <imdeepsv@gmail.com>
2026-01-29 22:14:47 +0530

    Fix: sidebar overflow issue

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -8,7 +8,7 @@ if (isSessionTimedOut()) {
 ?>
 
 <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-dark sidebar collapse">
-    <div class="position-sticky pt-5">
+    <div class="sidebar-sticky">
         <ul class="nav flex-column">
             <li class="nav-item">
                 <a class="nav-link <?php echo $active_menu === 'dashboard' ? 'active' : ''; ?>" href="/">
