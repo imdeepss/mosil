148b223a6632df815480b58eb192ffbd775398c8
imdeepss <imdeepsv@gmail.com>
2026-05-27 23:37:41 +0530

    feat: add newsroom page with content and configuration support

--- pages/newsroom.php
+++ pages/newsroom.php
@@ -454,8 +454,8 @@ $faqs = [
                             <p
                                 class="font-normal text-[16px] leading-[150%] tracking-[0.015em] text-[#757575] mb-2 line-clamp-3">
                                 <?php
-                                $content = clean_content($blog['content']);
-                                // $content = trim(preg_replace('/\s+/', ' ', strip_tags($content)));
+                                $content = trim(preg_replace('/\s+/', ' ', strip_tags($blog['content'])));
+                                $content = clean_content($content);
                                 echo substr($content, 0, 500);
                                 ?>
                             </p>
